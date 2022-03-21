9dc437921620c3eb3b3555ed57eb52fc62444adf
Kiran <kiranhulawale123@gmail.com>
2022-03-21 20:57:34 +0530

    Distance functional program is added

---
+++ src/com/bridgelabz/Assignments/Quadratic.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.Assignments;
+import java.util.Scanner;
+
+public class Quadratic {
+
+    static void roots(int a, int b, int c) {
+        // calculating delta value
+        int delta = Math.abs(b * b - 4 * a * c);
+        double x1 = (-b + Math.pow(delta, 1 / 2)) / (2 * a);
+        double x2 = (-b - Math.pow(delta, 1 / 2)) / (2 * a);
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        try {
+            System.out.println("Enter value for a ");
+            int a = s.nextInt();
+            System.out.println("Enter value for b");
+            int b = s.nextInt();
+            System.out.println("Enter value for c");
+            int c = s.nextInt();
+            roots(a, b, c);
+        } catch (Exception e) {
+            System.out.println("Enter correct input");
+        }
+        s.close();
+    }
+}
